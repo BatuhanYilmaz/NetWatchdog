2566a9146dbf7c396c44c07a1d12cb66c4294b96
Batuhan YILMAZ <batuhanyilmaz@marun.edu.tr>
2021-12-03 14:17:42 +0300

    Added ping feature
    - Added pinging to google.com feature to check the connection to the Internet
    - Updated logstep function to take one more additional argument
    - Deleted verbose parts

--- NetWatchdog/NetWatchdog.sh
+++ NetWatchdog/NetWatchdog.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-# Current date
-# CURR_DATE=`date`
+# ------ DEFINITION OF PARAMETERS START ------
 
+# Host to ping
+PING_HOST="google.com"
 # Name of log file
 LOGFILE=/home/ubuntu/NetWatchdog/netLogs.txt
 # Maximum number of retries
@@ -14,8 +15,12 @@ CURR_NETWORK=""
 # Definition of *off* and *no connection* states
 OFF_STATE="off/any"
 
+# ------ DEFINITION OF PARAMETERS END ------
+
+# ------ DEFINITION OF FUNCTIONS START ------
+
 function logStep {
-	echo "[" $(date +"%Y-%m-%d %T") "]" $1 $2 >> $LOGFILE
+	echo "[" $(date +"%Y-%m-%d %T") "]" $1 $2 $3 >> $LOGFILE
 }
 
 # Check the SSID of currently connected network
@@ -30,35 +35,35 @@ function checkWLAN {
 # Reset function for WLAN adapter
 function resetWLAN {
 	{
-		logStep "Resetting WLAN adapter..." ""
+		logStep "Resetting WLAN adapter..."
+		# Shutting the WLAN adapter down
 		sudo nmcli radio wifi off
-		logStep "WLAN adapter switched OFF" ""
-		# Waiting for the reset
+		logStep "WLAN adapter switched OFF"
+		# Waiting for the adapter to reset
 		sleep $WLAN_RESET_WAIT;
+		# Setting the adapter to ON state
 		sudo nmcli radio wifi on
-		logStep "WLAN adapter switched ON" ""
-		logStep "WLAN reset successful" ""
+		logStep "WLAN adapter switched ON"
+		logStep "WLAN reset successful"
 		#$(date +"%Y-%m-%d %T")
 		#sudo systemctl restart NetworkManager
 	} &> /dev/null
 }
 
-#function getCurrDate {
-#	CURR_DATE=date +"%Y-%m-%d %T"
-#}
+# ------ DEFINITION OF FUNCTIONS END ------
 
 # ----- MAIN BLOCK START ------
 
-logStep "------ WLAN Connection Check Routine START ------" ""
+logStep "------ WLAN Connection Check Routine START ------"
 
+# Check whether that the device is currently connected to a network
 checkWLAN
 sleep 1;
 
-# Check whether the device is connected wo a network or not 
+# Check whether the device is connected with a network or not 
 if [ ${CURR_NETWORK} != ${OFF_STATE} ]; then
 	echo "Current network SSID: " ${CURR_NETWORK}
 else
-
 	echo "NO NETWORK CONNECTION!"
 	# Retrying if reset fails
 	for ((i=0;i<$RETRY_MAX;i++)); do
@@ -71,11 +76,23 @@ else
 		fi
 	done
 	
+	# Ping a specified online host
+	
+	logStep "Pinging " ${PING_HOST} "..."
+	PING_RESULT=$(ping -c 1 google.com)
+	
+	# Check if the connection is established with the specified host
+	if [ $PING_RESULT -ne 0 ]; then
+		logStep "Unable to connect" ${PING_HOST} 
+	else
+		logStep "Connected successfully to" ${PING_HOST} 
+	fi
+	
 	# Unable to connect after *n* replies
 	#echo "[" $(date +"%Y-%m-%d %T") "]" "Unable to connect after "  $RETRY_MAX " retries" >> $LOGFILE
 
 fi
 
-logStep "------ WLAN Connection Check Routine END ------" ""
+logStep "------ WLAN Connection Check Routine END ------"
 
 # ------ MAIN BLOCK END ------
